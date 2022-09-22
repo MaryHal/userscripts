00db9732234894b74c85f59c82b03a907bf9eb84
MHL <mhl@theabsolute.plus>
2022-09-21 23:16:10 -0700

    Show reward data on non-battle pages

--- cb2_FightRewardLogger.js
+++ cb2_FightRewardLogger.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Fight Reward Logger
 // @namespace    http://tampermonkey.net/
-// @version      0.2.2
+// @version      0.2.5
 // @description
 // @author       You
 // @match        http://www.carnageblender.com/fight.tcl*
@@ -45,6 +45,44 @@
     );
   }
 
+  function showData(elementToAddTo, expData, cashData) {
+    const expAverage = expData.reduce((v, acc) => v + acc, 0) / expData.length;
+    const cashAverage =
+      cashData.reduce((v, acc) => v + acc, 0) / cashData.length;
+
+    const cashDataElement = window.document.createElement("input");
+    cashDataElement.setAttribute("readonly", "true");
+    cashDataElement.style.width = "100%";
+    cashDataElement.value = cashData.join(" ");
+    elementToAddTo.prepend(cashDataElement);
+
+    const averageCashElement = window.document.createElement("p");
+    averageCashElement.innerText = `Average $CB (${cashData.length}): ${cashAverage}`;
+    elementToAddTo.prepend(averageCashElement);
+
+    const expDataElement = window.document.createElement("input");
+    expDataElement.setAttribute("readonly", "true");
+    expDataElement.style.width = "100%";
+    expDataElement.value = expData.join(" ");
+    elementToAddTo.prepend(expDataElement);
+
+    const averageExpElement = window.document.createElement("p");
+    averageExpElement.innerText = `Average EXP (${expData.length}): ${expAverage}`;
+    elementToAddTo.prepend(averageExpElement);
+
+    const resetButton = window.document.createElement("button");
+    resetButton.innerText = "Reset";
+    resetButton.addEventListener(
+      "click",
+      function () {
+        saveRewardData(opponentId, RewardType.EXP, []);
+        saveRewardData(opponentId, RewardType.CASH, []);
+      },
+      false
+    );
+    elementToAddTo.prepend(resetButton);
+  }
+
   //window.addEventListener('load', function() {
   const params = new Proxy(new URLSearchParams(window.location.search), {
     get: (searchParams, prop) => searchParams.get(prop),
@@ -58,56 +96,20 @@
     "body > table:nth-child(4) > tbody > tr > td > table:nth-child(1) > tbody > tr:nth-child(3) > td > table > tbody > tr > td:nth-child(1) > table > tbody > tr:nth-child(4) > td:nth-child(2)"
   );
 
-  if (!cashReward || !expReward) {
-    console.log("No reward data on page");
-    return;
-  }
-
   const expData = loadRewardData(opponentId, RewardType.EXP);
   const cashData = loadRewardData(opponentId, RewardType.CASH);
 
-  expData.push(cleanNumber(expReward.innerText));
-  cashData.push(cleanNumber(cashReward.innerText));
-
-  saveRewardData(opponentId, RewardType.EXP, expData);
-  saveRewardData(opponentId, RewardType.CASH, cashData);
-
-  const expAverage = expData.reduce((v, acc) => v + acc, 0) / expData.length;
-  const cashAverage = cashData.reduce((v, acc) => v + acc, 0) / cashData.length;
-
   const body = window.document.getElementsByTagName("body")[0];
 
-  const cashDataElement = window.document.createElement("input");
-  cashDataElement.setAttribute("readonly", "true");
-  cashDataElement.style.width = "100%";
-  cashDataElement.value = cashData.join(" ");
-  body.prepend(cashDataElement);
+  if (cashReward && expReward) {
+    expData.push(cleanNumber(expReward.innerText));
+    cashData.push(cleanNumber(cashReward.innerText));
 
-  const averageCashElement = window.document.createElement("p");
-  averageCashElement.innerText = `Average $CB (${cashData.length}): ${cashAverage}`;
-  body.prepend(averageCashElement);
+    saveRewardData(opponentId, RewardType.EXP, expData);
+    saveRewardData(opponentId, RewardType.CASH, cashData);
+  }
 
-  const expDataElement = window.document.createElement("input");
-  expDataElement.setAttribute("readonly", "true");
-  expDataElement.style.width = "100%";
-  expDataElement.value = expData.join(" ");
-  body.prepend(expDataElement);
-
-  const averageExpElement = window.document.createElement("p");
-  averageExpElement.innerText = `Average EXP (${expData.length}): ${expAverage}`;
-  body.prepend(averageExpElement);
-
-  const resetButton = window.document.createElement("button");
-  resetButton.innerText = "Reset";
-  resetButton.addEventListener(
-    "click",
-    function () {
-      saveRewardData(opponentId, RewardType.EXP, []);
-      saveRewardData(opponentId, RewardType.CASH, []);
-    },
-    false
-  );
-  body.prepend(resetButton);
+  showData(body, expData, cashData);
 
   //}, false);
 })();
